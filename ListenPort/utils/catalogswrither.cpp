7367e5745bdb50f3510806f43acbfed61d12a404
Evgeniykull <Evgeniykull@yandex.ru>
2017-06-23 22:28:27 +0300

    Добавлен функционал получения каталога из файла

--- ListenPort/utils/catalogswrither.cpp
+++ ListenPort/utils/catalogswrither.cpp
@@ -25,17 +25,18 @@ void catalogswrither::writeCatalogToFile() {
     dir.setSorting(QDir::Name);
     QFileInfoList files_list = dir.entryInfoList();
 
-    std::vector<QString> name_mass;
-    std::vector<QString> type_mass;
-    std::vector<QString> len_mass;
-    std::vector<QString> addr_mass;
-    std::vector<QString> data_mass;
+    //std::vector<QString> name_mass;
+    std::vector<QByteArray> name_mass;
+    std::vector<QByteArray> type_mass;
+    std::vector<QByteArray> len_mass;
+    std::vector<QByteArray> addr_mass;
+    std::vector<QByteArray> data_mass;
     for (int i = 0; i < files_list.size(); ++i)
     {
         QFileInfo fileInfo = files_list.at(i);
 
         //type
-        QString  file_type = fileInfo.completeSuffix();
+        QByteArray file_type = fileInfo.completeSuffix().toLatin1();
         if (file_type.length() >= 4) {
             file_type = file_type.left(4);
         } else {
@@ -46,7 +47,7 @@ void catalogswrither::writeCatalogToFile() {
         type_mass.push_back(file_type);
 
         //name
-        QString file_name = fileInfo.completeBaseName();
+        QByteArray file_name = fileInfo.completeBaseName().toLatin1();
         if (file_name.length() >= 19) {
             file_name = file_name.left(19);
         } else {
@@ -64,7 +65,7 @@ void catalogswrither::writeCatalogToFile() {
             fsize = fsize / 256;
         }
 
-        QString file_len;
+        QByteArray file_len;
         file_len[0] = ssi[3];
         file_len[1] = ssi[2];
         file_len[2] = ssi[1];
@@ -76,8 +77,8 @@ void catalogswrither::writeCatalogToFile() {
         if (i == 0) {
             abs_addr = files_list.size() * 32 + 32;
         } else {
-            QByteArray ba = addr_mass[i-1].toLatin1();
-            QByteArray ba2 = len_mass[i-1].toLatin1();
+            QByteArray ba = addr_mass[i-1];
+            QByteArray ba2 = len_mass[i-1];
             unsigned int _addr, _len;
             unsigned char sym3 = ba[0];
             unsigned char sym2 = ba[1];
@@ -118,7 +119,7 @@ void catalogswrither::writeCatalogToFile() {
             abs_addr = abs_addr / 256;
         }
 
-        QString addr;
+        QByteArray addr;
         addr[0] = ssi1[3];
         addr[1] = ssi1[2];
         addr[2] = ssi1[1];
@@ -126,7 +127,7 @@ void catalogswrither::writeCatalogToFile() {
         addr_mass.push_back(addr);
 
         //собираем данные
-        QString data_part;
+        QByteArray data_part;
         data_part += addr_mass[i];
         data_part += len_mass[i];
         data_part += type_mass[i];
@@ -140,24 +141,33 @@ void catalogswrither::writeCatalogToFile() {
     {
         qDebug() << "Ошибка при открытии файла";
     }
+
     //запись информации о файлах
+    QByteArray info_ba;
     for (int i = 0; i < data_mass.size(); i++) {
-        file.write(data_mass[i].toStdString().c_str());
+        info_ba.append(data_mass[i]);
     }
+    file.write(info_ba);
+
     //запись нулевой строки
+    QByteArray null_string;
     for (int i = 0; i < 32; i++) {
-        file.write("0");
+        null_string.append("0");
     }
+    file.write(null_string);
+
     //запись данных
     for (int i = 0; i < files_list.size(); ++i) {
         QString path = files_list.at(i).absoluteFilePath();
-        QFile *f = new QFile(path);
-        if (!f->open(QIODevice::ReadOnly))
+        QFile f(path);
+        if (!f.open(QIODevice::ReadOnly))
         {
             qDebug() << "Ошибка при открытии файла в каталоге";
         }
-        QByteArray ba = f->readAll();
+        QByteArray ba;
+        ba = f.readAll();
         file.write(ba);
+        f.close();
     }
     file.close();
 }
@@ -193,29 +203,29 @@ void catalogswrither::getCatalogByFile() {
         null_string.append("0");
     }
     int null_pos = file_data.indexOf(null_string);
-
-    QString files_info = file_data.left(null_pos);
+    while (file_data[null_pos + 32] == '0') { //дальнейший поиск первого символа, который не 0
+        null_pos++;
+    }
+    QByteArray files_info = file_data.left(null_pos);
     int files_num = files_info.length() / 32;
     for (int k = 0; k < files_num; k++) {
-        QString fil = files_info.mid(k * 32, 32);
+        QByteArray fil = files_info.mid(k * 32, 32);
         //разбор файла
-        QString f_pos = fil.left(4);
-        QString f_len = fil.mid(4, 4);
-        QString f_type = fil.mid(8, 4);
-        QString f_name = fil.mid(12, 19);
+        QByteArray f_pos = fil.left(4);
+        QByteArray f_len = fil.mid(4, 4);
+        QByteArray f_type = fil.mid(8, 4);
+        QByteArray f_name = fil.mid(12, 19);
 
-        //char * _f_ps = f_pos.toStdString().c_str();
-        unsigned char sym3 = f_pos[3].toLatin1();
-        unsigned char sym2 = f_pos[2].toLatin1();
-        unsigned char sym1 = f_pos[1].toLatin1();
-        unsigned char sym0 = f_pos[0].toLatin1();
+        unsigned char sym3 = f_pos[0];
+        unsigned char sym2 = f_pos[1];
+        unsigned char sym1 = f_pos[2];
+        unsigned char sym0 = f_pos[3];
         unsigned int _f_pos = (sym3 << 24) + (sym2 << 16) + (sym1 << 8) + sym0;
-        //char * _f_ln = f_len.toStdString().c_str();
-        sym3 = f_len[3].toLatin1();
-        sym2 = f_len[2].toLatin1();
-        sym1 = f_len[1].toLatin1();
-        sym0 = f_len[0].toLatin1();
-        unsigned int _f_len = (sym3 << 24) + (sym2 << 16) + (sym1 << 8) + sym0;
+        sym3 = f_len[0];
+        sym2 = f_len[1];
+        sym1 = f_len[2];
+        sym0 = f_len[3];
+        unsigned int _f_len = (sym3 << 24) + (sym2 << 16) + (sym1 << 8) + sym0; //ошибки с длинной
 
         QString full_name = f_name + "." + f_type;
         QFile file(dir_path + "/" + full_name);
@@ -223,7 +233,8 @@ void catalogswrither::getCatalogByFile() {
         {
             qDebug() << "Ошибка при создании файла в каталоге";
         }
-        file.write(file_data.mid(_f_pos, _f_len).toStdString().c_str());
+        QByteArray select_file_data = file_data.mid(_f_pos, _f_len); //не все верно записывается!!!
+        file.write(select_file_data);
         file.close();
     }
 }
